f3625405e8b615396b6ad50faf6235fd5f84c574
GYUHO <165417701+KU-ARL@users.noreply.github.com>
2025-12-24 18:40:32 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int sum=0;
+	int arr[6];
+
+	for(int i=0;i<5;i++) {
+		cin >> arr[i];
+	}
+
+	for(int i=0;i<5;i++) {
+		sum += arr[i];
+	}
+
+	sort(arr, arr+5);
+
+	cout << sum/5 << '\n';
+	cout << arr[2];
+}
