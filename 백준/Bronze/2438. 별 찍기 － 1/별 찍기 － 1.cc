6a92cae7c52efc74ab337fdf8e4b56b48e009b50
GYUHO <165417701+KU-ARL@users.noreply.github.com>
2025-12-25 11:44:00 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	for(int i=1;i<N+1;i++) {
+		for(int j=1;j<i+1;j++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+}
