a2d21363aa16ce0f913ffd948980ef1c7505d545
GYUHO <165417701+KU-ARL@users.noreply.github.com>
2025-12-25 10:37:46 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -6,34 +6,27 @@ int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int num[9];
-	int sum = 0;
+	int arr[9];
+	int sum=0;
+
+	for (int i=0;i<9;i++) cin >> arr[i];
+	sort(arr, arr+9);
+
 	for (int i=0;i<9;i++) {
-		cin >> num[i];
-	}
-
-	sort(num, num+9);
-
-	for( int i=0 ; i<9 ; i++) {
-		for (int j=0;j<9;j++) {
-			if (i != j) {
-				for (int k=0; k<9; k++ ) {
-					if (k != i && k !=j) {
-						sum += num[k];
-					}
-				}
-
-				if (sum == 100) {
-					for (int l=0; l<9;l++) {
-						if (l != i && l !=j) {
-							cout << num[l] << '\n';
-						}
-					}
-					return 0;
-				}
-
-				sum=0;
+		for (int j=i+1;j<9;j++) {
+			for (int k=0;k<9;k++) {
+				if(k!=i && k!=j) sum += arr[k];
 			}
+			
+			if (sum == 100) {				
+				for (int k=0;k<9;k++) {
+					if(k!=i && k!=j) cout << arr[k] << '\n';
+				}
+
+				return 0;
+			}
+
+			sum = 0;
 		}
 	}
 }
