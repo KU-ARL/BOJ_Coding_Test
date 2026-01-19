d4789066c5256bc10db1160c6bf227ef2fed2c1c
GYUHO <165417701+KU-ARL@users.noreply.github.com>
2026-01-19 12:35:49 +0900

    [Silver II] Title: 에디터, Time: 32 ms, Memory: 7576 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,41 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+const int MX = 600005;
+char dat[MX]; 
+int pre[MX], nxt[MX];
+int unused = 1;
+
+
+void insert(int addr, char num) {
+  dat[unused]=num;
+  nxt[unused]=nxt[addr];
+  pre[unused]=addr;
+
+  if(nxt[addr]!=-1) pre[nxt[addr]]=unused;
+  nxt[addr]=unused;
+
+  unused++;
+}
+
+void erase(int addr) {
+  if(nxt[addr]!=-1) pre[nxt[addr]]=pre[addr];
+  nxt[pre[addr]]=nxt[addr];
+}
+
+void traverse() {
+  int cur = nxt[0];
+  while(cur!=-1) {
+    cout << dat[cur];
+    cur = nxt[cur];
+  }
+}
+
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  list<char> L;
+  fill(pre, pre+MX, -1);
+  fill(nxt, nxt+MX, -1);
+
   string S;
   cin >> S;
-  
-  for(auto s: S) L.push_back(s);
 
-  list<char>::iterator t=L.end();
+  int t = 0;
+
+  for(auto s: S) {
+    insert(t, s);
+    t=nxt[t];
+  }
 
   int N;
   cin >> N;
 
   for(int i=0; i<N; i++) {
-    char C;
-    cin >> C;
+    char input;
+    cin >> input;
 
-    if (C == 'L') {
-      if(t!=L.begin()) t--;
-    } else if (C == 'D') {
-      if(t!=L.end()) t++;
-    } else if (C == 'B') {
-      if(t!=L.begin()) {
-        t--;
-        t = L.erase(t);
+    if(input=='L') {
+      if(pre[t]!=-1) t=pre[t];
+    } else if (input=='D') {
+      if(nxt[t]!=-1) t=nxt[t];
+    } else if (input=='B') {
+      if(t!=0) {
+        erase(t);
+        t=pre[t];
       }
-    } else if (C == 'P') {
-      char input;
-      cin >> input;
+    } else if (input=='P') {
+      char w;
+      cin >> w;
 
-      L.insert(t, input);
+      insert(t, w);
+      t=nxt[t];
     }
   }
 
-  for(auto i: L) cout << i;
+  traverse();
+
 }
