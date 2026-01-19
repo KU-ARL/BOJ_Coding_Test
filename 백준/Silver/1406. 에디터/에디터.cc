a48ef72330bfd0c1dde31fc5b20adff81bdc0a3e
GYUHO <165417701+KU-ARL@users.noreply.github.com>
2026-01-19 11:31:44 +0900

    [Silver II] Title: 에디터, Time: 60 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,37 +1,41 @@
-#include <iostream>
-#include <list>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-	string s;
+  list<char> L;
+  string S;
+  cin >> S;
+  
+  for(auto s: S) L.push_back(s);
 
-	cin >> s;
+  list<char>::iterator t=L.end();
 
-	list<char> L;
-	for(auto c : s) L.push_back(c);
-	auto cursor = L.end();
-	int q;
-	cin >> q;
-	while(q--) {
-		char op;
-		cin >> op;
-		if(op=='P') {
-			char add;
-			cin >> add;
-			L.insert(cursor, add);
-		} else if (op=='L') {
-			if (cursor!=L.begin()) cursor--;
-		} else if(op=='D') {
-			if(cursor!=L.end()) cursor++;
-		} else {
-			if(cursor!=L.begin()) {
-				cursor--;
-				cursor = L.erase(cursor);
-			}
-		}
-	}
-	for(auto c:L) cout << c;
+  int N;
+  cin >> N;
+
+  for(int i=0; i<N; i++) {
+    char C;
+    cin >> C;
+
+    if (C == 'L') {
+      if(t!=L.begin()) t--;
+    } else if (C == 'D') {
+      if(t!=L.end()) t++;
+    } else if (C == 'B') {
+      if(t!=L.begin()) {
+        t--;
+        t = L.erase(t);
+      }
+    } else if (C == 'P') {
+      char input;
+      cin >> input;
+
+      L.insert(t, input);
+    }
+  }
+
+  for(auto i: L) cout << i;
 }
