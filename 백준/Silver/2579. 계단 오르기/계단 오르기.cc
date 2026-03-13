f5ced7da4937edf7d30063d6c3d7b1cedbe78edd
GYUHO <165417701+KU-ARL@users.noreply.github.com>
2026-03-13 23:38:42 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int s[305];
-int d[305][3];
+int d[305];
 
 int main(void) {
     ios::sync_with_stdio(0);
@@ -11,18 +11,20 @@ int main(void) {
     int N;
     cin >> N;
 
+    int sum = 0;
+
     for(int i=1;i<=N;i++) {
         cin >> s[i];
+        sum += s[i];
     }
 
-    d[1][1] = s[1];
-    d[2][1] = s[2];
-    d[2][2] = s[1] + s[2];
+    d[1] = s[1];
+    d[2] = s[2];
+    d[3] = s[3];
 
-    for(int i=3;i<=N;i++) {
-        d[i][1] = max(d[i-2][1], d[i-2][2]) + s[i];
-        d[i][2] = d[i-1][1] + s[i];
+    for(int i=4;i<=N;i++) {
+        d[i] = min(d[i-3], d[i-2]) + s[i];
     }
     
-    cout << max(d[N][1], d[N][2]);
+    cout << sum - min(d[N-1], d[N-2]);
 }
