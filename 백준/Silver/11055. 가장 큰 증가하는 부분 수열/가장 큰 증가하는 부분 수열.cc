0c6222d9b0b0f0575f31b0ed628bfb848066649f
GYUHO <165417701+KU-ARL@users.noreply.github.com>
2026-03-18 13:59:12 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -1,8 +1,9 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int num[1002];
-int sum[1002];
+int num[1005];
+int sum[1005];
 
 int main(void) {
     ios::sync_with_stdio(0);
@@ -10,21 +11,24 @@ int main(void) {
 
     int N;
     cin >> N;
-    for(int i=1; i<=N; i++) {
+
+    for (int i=1;i<=N;i++) {
         cin >> num[i];
         sum[i] = num[i];
     }
-    
-    for(int i=1; i<=N; i++) {
-        for (int j=1; j<i;j++) {
-            if(num[j]<num[i]) {
-                sum[i] = max(sum[i], num[i]+sum[j]);
+
+    for (int i=1;i<=N;i++) {
+        for (int j=1;j<i;j++) {
+            if(num[j] < num[i]) {
+                sum[i] = max(sum[i], sum[j]+num[i]);
             }
         }
-
     }
 
+
     int result = 0;
-    for(int i=1;i<=N;i++) result = max(result, sum[i]);
+    for (int i=1;i<=N;i++) result = max(result, sum[i]);
     cout << result;
+
+
 }
