4b258374f81347cd52cd15683db3bdfe012552b4
GYUHO <165417701+KU-ARL@users.noreply.github.com>
2026-03-19 13:25:27 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2244 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -7,8 +7,8 @@ using namespace std;
 int board[105][105];
 int vis[105][105];
 
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, -1, 0, 1};
+int dx[4]={1, 0, -1, 0};
+int dy[4]={0, 1, 0, -1};
 
 int main(void) {
     ios::sync_with_stdio(0);
@@ -21,7 +21,7 @@ int main(void) {
         string input;
         cin >> input;
 
-        for(int j=0; j<input.size();j++) {
+        for(int j=0; j<input.size(); j++) {
             board[i][j] = input[j] - '0';
         }
     }
@@ -29,23 +29,22 @@ int main(void) {
     queue<pair<int, int>> Q;
     Q.push({0, 0});
     vis[0][0] = 1;
-    
+
     while(!Q.empty()) {
         pair<int, int> cur = Q.front();
         Q.pop();
 
-        for(int i=0;i<4;i++) {
+        for(int i=0; i<4; i++) {
             int nx = cur.X + dx[i];
             int ny = cur.Y + dy[i];
 
-            if(nx <0 || nx >= n || ny <0 || ny >=m) continue;
-            if(board[nx][ny] == 0 || vis[nx][ny] != 0) continue;
+            if(nx <0 || nx >= n || ny <0 || ny >=m)  continue;
+            if(board[nx][ny]==0 || vis[nx][ny]!=0)  continue;
 
             Q.push({nx, ny});
-            vis[nx][ny] = vis[cur.X][cur.Y]+1;
+            vis[nx][ny] = vis[cur.X][cur.Y] + 1;
         }
     }
 
     cout << vis[n-1][m-1];
-
 }
