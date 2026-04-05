f60494bb181f19ab004b8d63f3a7a5ee3df3414a
GYUHO <165417701+KU-ARL@users.noreply.github.com>
2026-04-05 23:15:34 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int board[17][17];
-int vis[17][17];
+int vis[17][17][3];
 
 int main(void) {
     ios::sync_with_stdio(0);
@@ -17,81 +17,20 @@ int main(void) {
         }
     }
 
-    queue<pair<int, pair<int, int>>> Q; // {방향, {좌표}}
-    Q.push({1, {1, 2}});                  // 가로방향 + 시작점 위치
+    vis[1][2][1] = 1;
 
-    while(!Q.empty()) {
-
-        pair<int, pair<int, int>> cur = Q.front();
-        Q.pop();
-
-        int dir = cur.first;
-        int dx = cur.second.first;
-        int dy = cur.second.second;
-
-        // cout << dir << ',' << dx << ',' << dy << '\n';
-
-        if (dir == 1) {
-            if (dx+1 <= N && (dx+1 <= N && dy+1 <=N) && dy+1 <=N) {  // 대각선 가능한가
-                if (board[dx][dy+1] + board[dx+1][dy+1] + board[dx+1][dy] == 0) {
-                    Q.push({3, {dx+1, dy+1}});
-                    vis[dx+1][dy+1] += 1;
-
-                    // cout << dir << ',' << dx << ',' << dy << "1&대각\n";
-                }
-            }
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=N; j++) {
+            if(board[i][j] == 1) continue;
             
-            if (dy+1 <= N && board[dx][dy+1] == 0) {  // 옆으로 가능한가
-                Q.push({1, {dx, dy+1}});
-                vis[dx][dy+1] +=1;
-                // cout << dir << ',' << dx << ',' << dy << "1&옆\n";
+            if(j-1>0 && board[i][j-1] == 0) vis[i][j][1] += vis[i][j-1][1] + vis[i][j-1][3];
 
-            }
+            if(i-1>0 && board[i-1][j] == 0) vis[i][j][2] += vis[i-1][j][2] + vis[i-1][j][3];
 
-            // cout << dir << ',' << dx << ',' << dy << "1끝\n";
+            if(j-1>0 && i-1>0 && board[i-1][j]+board[i][j-1] == 0) vis[i][j][3] += vis[i-1][j-1][1] + vis[i-1][j-1][2] + vis[i-1][j-1][3];
 
-
-        } else if (dir == 2) {  // 세로
-            if (dx+1 <= N && (dx+1 <= N && dy+1 <=N) && dy+1 <=N) {  // 대각선 가능한가
-                if (board[dx][dy+1] + board[dx+1][dy+1] + board[dx+1][dy] == 0) {
-                    Q.push({3, {dx+1, dy+1}});
-                    vis[dx+1][dy+1] +=1;
-                    // cout << dir << ',' << dx << ',' << dy << "2&대각\n";
-                }
-            }
-            
-            if (dx+1 <= N && board[dx+1][dy] == 0) {  // 아래로 가능한가
-                Q.push({2, {dx+1, dy}});
-                vis[dx+1][dy] +=1;
-                // cout << dir << ',' << dx << ',' << dy << "2&아래\n";
-            }
-
-            // cout << dir << ',' << dx << ',' << dy << "2끝\n";
-        } else if (dir == 3) {  // 대각선
-            if (dx+1 <= N && (dx+1 <= N && dy+1 <=N) && dy+1 <=N) {  // 대각선 가능한가
-                if (board[dx][dy+1] + board[dx+1][dy+1] + board[dx+1][dy] == 0) {
-                    Q.push({3, {dx+1, dy+1}});
-                    vis[dx+1][dy+1] +=1;
-                    // cout << dir << ',' << dx << ',' << dy << "3&대각\n";
-                }
-            }
-            
-            if (dx+1 <= N && board[dx+1][dy] == 0) {  // 아래로 가능한가
-                Q.push({2, {dx+1, dy}});
-                vis[dx+1][dy] +=1;
-                // cout << dir << ',' << dx << ',' << dy << "3&아래\n";
-            }
-
-                        
-            if (dy+1 <= N && board[dx][dy+1] == 0) {  // 옆으로 가능한가
-                Q.push({1, {dx, dy+1}});
-                vis[dx][dy+1] +=1;
-                // cout << dir << ',' << dx << ',' << dy << "3&옆\n";
-            }
-
-            // cout << dir << ',' << dx << ',' << dy << "3끝\n";
         }
     }
 
-    cout << vis[N][N];
+    cout << vis[N][N][1] + vis[N][N][2] + vis[N][N][3];
 }
